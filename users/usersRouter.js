cc8a21f244dc08b42eaa28790cb0f3e3d6377c58
Babatunde <babatundea15@gmail.com>
2020-06-14 18:46:23 +0100

    created the login endpoint

---
+++ users/usersRouter.js
@@ -0,0 +1,47 @@
+// Bring in express
+const express = require("express");
+// We will need bcryptjs for hashing passwords
+const bcrypt = require("bcrypt");
+// Bring in the helper functions from usersDB
+const users = require("./usersDB");
+// Import the router
+const router = express.Router();
+
+// Users endpoints here 👇👇👇
+
+// This is the register endpoint
+router.post("/register", (req, res) => {
+  const { username, password, isManager } = req.body;
+  //   We hash the password
+  const bcryptHash = bcrypt.hashSync(password, 10);
+  const newUser = { username, bcryptHash, isManager };
+  users
+    .addUser(newUser)
+    .then((member) => {
+      res.status(201).json({ message: `User created successfully` });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
+});
+
+router.post("/login", (req, res) => {
+  const auth_user = req.body;
+  //   We need to first fish out the user from the db
+  users
+    .getUserBy(auth_user.username)
+    .then((member) => {
+      if (member && bcrypt.compareSync(auth_user.password, member.password)) {
+        res
+          .status(200)
+          .json({ message: `Logged in successfully, ${member.username}` });
+      } else {
+        res.status(500).json({ message: `Credentials are not valid` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
+});
+// The router should be exported
+module.exports = router;
