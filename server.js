21f2fe7fb16166bcc9c7ef5ae17a1b1562c35bb1
Babatunde <babatundea15@gmail.com>
2020-06-14 06:51:29 +0100

    created a custom logger middleware

--- server.js
+++ server.js
@@ -9,12 +9,28 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(logger);
 // server.use("/api/form", formRouter);
 
 // Flesh out a dummy API
 server.get("/", (req, res) => {
   res.send("<h2>Welcome to Cynorlabs!</h2>");
 });
+
+// If the endpoint is invalid
+server.get("*", (req, res) => {
+  res.status(404).json({ message: "This is an invalid path" });
+});
+
+// Create a custom logger middleware
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString}] ${req.method} ${req.url} from ${req.get(
+      "Origin"
+    )}`
+  );
+  next();
+}
 
 // Export the server to be seen by other files
 module.exports = server;
